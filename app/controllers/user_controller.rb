7ec5329f09eaba6366bb23b4d0719d43d27dd67f
forsrc <forsrc@163.com>
2016-07-29 14:14:35 +0900

    create show index

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,7 +7,28 @@ class UserController < ApplicationController
 
     # create
     def create
-        render plain: params[:user].inspect
+        # render plain: params[:user].inspect
+
+        @user = User.new(user_params_new)
+        @user.save
+        redirect_to @user
+
     end
+
+
+    def show
+        # render plain: params[:id].inspect
+        @user = User.find(params[:id])
+    end
+
+    def index
+        @users = User.all
+    end
+
+
+    private
+      def user_params_new
+        params.require(:user).permit(:username, :password, :email)
+      end
 
 end
