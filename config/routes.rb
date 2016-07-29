d486bdcd545f9015f740399ab2da4bb363f26299
forsrc <forsrc@163.com>
2016-07-29 17:04:05 +0900

    controller: user -> users

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/index'
 
-
-  resources :user
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
