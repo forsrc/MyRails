05e1eaf9bc21a030e42d7953f6371ae3a8a14d54
forsrc <forsrc@163.com>
2016-07-30 21:47:01 +0900

    validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
   validates_length_of :username, :minimum => 5, :maximum => 50
   validates_length_of :password, :in => 6..20
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => "Incorrect email format"
-  validates_presence_of :version
   validates_presence_of :status
   validates_inclusion_of :status, :in => 0..1, # %w{ 0 1 }
                          :message => "should be '0' -> (NG) or '1' -> (OK)"
@@ -16,11 +15,21 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 5 }
   validates :password, presence: true, length: { minimum: 6 }
   validates :email, presence: true, length: { minimum: 5 }
+  validates :version, presence: true,if: :fun_update_version_is_same?
 
   before_save :fun_create_on
   before_update :fun_update_on
 
   protected
+  def fun_update_version_is_same?
+
+    if version != self.version
+      errors.add(:version, "version " + version.to_s + " not " + self.version.to_s)
+      return
+    end
+
+  end
+
   def fun_update_on
     # self.update_on = DateTime.parse(Time.now.strftime('%Y-%m-%d %H:%M:%S.%N').to_s
     self.update_on = Time.now.strftime('%Y-%m-%d %H:%M:%S.%N')
